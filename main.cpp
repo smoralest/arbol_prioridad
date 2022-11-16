797857ab8f90f52aca89279ea9d2c2874f0a7dfb
Sergio-Vega-c <savegac@correo.udistrital.edu.co>
2022-11-15 20:34:54 -0500

    error uy

--- main.cpp
+++ main.cpp
@@ -2,29 +2,34 @@
 #include "priorityQueue.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+using namespace std; 
 int main(int argc, char** argv) {
 	priorityQueue<int> test(5);
 	
 	test.insertOnPriorityQueue(60);
-	test.insertOnPriorityQueue(80);
+	test.insertOnPriorityQueue(30);
 	test.insertOnPriorityQueue(20);
+	
 	test.insertOnPriorityQueue(90);
 	test.insertOnPriorityQueue(50);
 	test.insertOnPriorityQueue(40);
+/*	
+	
+
 	test.insertOnPriorityQueue(30);
 	test.insertOnPriorityQueue(70);
 	test.insertOnPriorityQueue(55);
 	test.insertOnPriorityQueue(45);
+	*/
 	
 //	for(int i = 1; i <= test.sizePriorityQueue();i++){
-		test.printPriorityQueue();
-//	}
-	cout<<"---------------------"<<endl;
-//		test.guardarArreglo();	
-	test.attendPriorityQueue();
-		cout<<"2---------------------"<<endl;
-//	test.printPriorityQueue();
-		cout<<"3---------------------"<<endl;
+//		test.printPriorityQueue();
+////	}
+//	cout<<"---------------------"<<endl;
+////		test.guardarArreglo();	
+//	test.attendPriorityQueue();
+//		cout<<"2---------------------"<<endl;
+////	test.printPriorityQueue();
+//		cout<<"3---------------------"<<endl;
 	return 0;
 }
--- priorityQueue.h
+++ priorityQueue.h
@@ -8,38 +8,35 @@
 #define PRIORITYQUEUE_H
 
 using namespace std;
-template <class T>
-struct NodoPQ{T info;
-	NodoPQ<T> *sig;
-	int pos;
-};
+
 
 template <class T>
-class priorityQueue{NodoPQ<T>*cab = new NodoPQ<T>;
+class priorityQueue{
 			int tam;
 			int pos2;
 			T arreglo [];
 			int pos;
 			T nc;
+			int espacio; 
 	public: priorityQueue(int a){
 			T Arreglo[a];
 			T *Arr = &Arreglo[0];
 			cout<<"Arreglo de tamaño "<<a<<" ha sido creado";
 			cout<<" en el espacio de memoria: "<<Arr<<endl;
-			cab=NULL;
 			tam=0;
 			pos2= 0;
 			pos= 1;
 			nc=0;
+			espacio = a; 
 //						cout<<"ndsdsrregloxxx:	"<<nc<<endl;
 			}
 
 			bool isEmptyPriorityQueue();
 			int sizePriorityQueue();
 			void insertOnPriorityQueue(T dato); 
-			void attendPriorityQueue();
+		//	void attendPriorityQueue();
 			T printPriorityQueue();
-			NodoPQ<T> *searchPosition(int);
+		//	NodoPQ<T> *searchPosition(int);
 			void guardarArreglo ();
 			void organizarArreglo();
 //			~priorityQueue();
@@ -51,133 +48,99 @@ int priorityQueue<T>::sizePriorityQueue(){
 	return tam;
 }	
 
-template <class T>
-void  priorityQueue<T>:: guardarArreglo(){
-	cout<<"xxxncc:	"<<nc<<endl;
-	NodoPQ<T> *ncambio2 = new NodoPQ<T>;
-	ncambio2 = cab;
-	arreglo[tam];
-	cout<<"xxx2ncc:	"<<nc<<endl;
-	for(int i = 0; i< tam;i++){
-		cout<<"xxxxxxxxncc:	"<<nc<<"	i:"<<i<<endl;
-		arreglo[i]= ncambio2->info;
+//template <class T>
+//void  priorityQueue<T>:: guardarArreglo(){
+//	cout<<"xxxncc:	"<<nc<<endl;
+////	NodoPQ<T> *ncambio2 = new NodoPQ<T>;
+//	ncambio2 = cab;
+//	arreglo[tam];
+//	cout<<"xxx2ncc:	"<<nc<<endl;
+//	for(int i = 0; i< tam;i++){
+//		cout<<"xxxxxxxxncc:	"<<nc<<"	i:"<<i<<endl;
+//		arreglo[i]= ncambio2->info;
+////		cout<<arreglo[i]<<endl;
+//		ncambio2 = ncambio2->sig;
+//	}
+//	cout<<"perra:	"<<arreglo[1]<<endl;
+//				cout<<"ncccccc:	"<<nc<<endl;
+//}	
+
+//template <class T>
+//T priorityQueue<T>::printPriorityQueue(){
+//				cout<<"ncsssss:	"<<nc<<endl;
+////	NodoPQ<T> *aux = new NodoPQ<T>;
+////	aux=cab;
+////	for(int i = 1; i<= tam;i++){
+////	
+////		if(aux->pos==lugar){
+////			cout<<"Informacion: "<<aux->info<<endl;
+////			return aux->info;
+////		}else{
+////			aux=aux->sig;
+////		}
+////	}
+////	return NULL;
+//	guardarArreglo();
+//	for(int i = 0; i< tam;i++){
 //		cout<<arreglo[i]<<endl;
-		ncambio2 = ncambio2->sig;
-	}
-	cout<<"perra:	"<<arreglo[1]<<endl;
-				cout<<"ncccccc:	"<<nc<<endl;
-}	
-template <class T>
-T priorityQueue<T>::printPriorityQueue(){
-				cout<<"ncsssss:	"<<nc<<endl;
+//	}
+//}
+
+//
+//template <class T>
+//NodoPQ<T> *priorityQueue<T>::searchPosition(int posicion){
 //	NodoPQ<T> *aux = new NodoPQ<T>;
 //	aux=cab;
-//	for(int i = 1; i<= tam;i++){
-//	
-//		if(aux->pos==lugar){
-//			cout<<"Informacion: "<<aux->info<<endl;
-//			return aux->info;
+//	while(aux->sig!=NULL){
+//		if(aux->pos==posicion){
+//			return aux;
 //		}else{
 //			aux=aux->sig;
 //		}
 //	}
-//	return NULL;
-	guardarArreglo();
-	for(int i = 0; i< tam;i++){
-		cout<<arreglo[i]<<endl;
-	}
-}
-
-
-template <class T>
-NodoPQ<T> *priorityQueue<T>::searchPosition(int posicion){
-	NodoPQ<T> *aux = new NodoPQ<T>;
-	aux=cab;
-	while(aux->sig!=NULL){
-		if(aux->pos==posicion){
-			return aux;
-		}else{
-			aux=aux->sig;
-		}
-	}
-	return cab;	
-}
+//	return cab;	
+//}
 	
 template <class T>
 void priorityQueue<T>::insertOnPriorityQueue(T dato){
-//	NodoPQ<T> *nn = new NodoPQ<T>;
-//	NodoPQ<T> *ncambio = new NodoPQ<T>;	
-//	NodoPQ<T> *ncambio2 = new NodoPQ<T>;
-//	nn->info=dato;
 
-	if(tam==0){
-		nn->sig = NULL;
-		cab = nn;
-		tam++;
-		nn->pos=tam;
-	}else{
-	NodoPQ<T> *aux = new NodoPQ<T>;
-	aux=cab;
-	while(aux->sig!=NULL){
-		aux=aux->sig;
+	if (tam==0){
+		// si no existe nada 
+		arreglo[0]=dato; 
+		cout<<arreglo[0]<<endl; 
+		tam++; 
+		cout<<"entro"<<endl;
 	}
-	aux->sig=nn;
-	nn->sig=NULL;
-	tam++;
-	nn->pos=tam;
-	int auxPos = tam;
-	while(auxPos > 0){
-		//hacemos el cambio
-		if(nn->info >= (searchPosition(auxPos/2)->info)){
-			if(searchPosition(auxPos/2) == cab){
-				ncambio->info = nn->info;
-				nn->info = cab->info;
-				cab->info = ncambio->info ;
-			}else{
-				ncambio->info = nn->info;
-				nn->info = searchPosition(auxPos/2)->info;
-				searchPosition(auxPos/2)->info = ncambio->info;	
-			}
+	if (tam >0 && tam < espacio){
+		// si se puede ingresar al arreglo 
+		  cout<<"espacio: "<<espacio<<"------";
+		  arreglo[tam]=dato;
+		  cout<<arreglo[tam]<<endl; 
+		  tam++;
 		
-		}else if(ncambio2->info > (searchPosition(auxPos/2)->info)){
-			
-			if(searchPosition(auxPos/2) == cab){
-				ncambio->info = ncambio2->info;
-				ncambio2->info = cab->info;
-				cab->info = ncambio->info ;
-			}else{
-				ncambio->info = nn->info;
-				nn->info = searchPosition(auxPos/2)->info;
-				searchPosition(auxPos/2)->info = ncambio->info;	
-			}		
-		}
-		ncambio2 = searchPosition(auxPos/2);
-		auxPos = auxPos/2;
-	}
-		
-
-		
+	}else {
+		// si no puede meter mas cosas 
+		cout<<"espacio lleno"<<endl; 
 		
 	}
-	
 	
 }
  
 
-template <class T>
-void priorityQueue<T>::attendPriorityQueue(){
-			cout<<"arregloxxx222:	"<<arreglo[1]<<endl;
-			cout<<"ncxxx:	"<<nc<<endl;
+//template <class T>
+//void priorityQueue<T>::attendPriorityQueue(){
+//			cout<<"arregloxxx222:	"<<arreglo[1]<<endl;
+//			cout<<"ncxxx:	"<<nc<<endl;
 //
 //	if(pos2==0){
 //							cout<<"arreglo22222:"<<endl;
 //								for(int i = 0; i< tam;i++){
 //		cout<<arreglo[i]<<endl;
 //	}	
-	nc=140;
-//		nc = arreglo[(tam-1)];
-//		arreglo[1]=30;
-		cout<<"arregloxxx:	"<<arreglo[1]<<endl;
+//	nc=140;
+////		nc = arreglo[(tam-1)];
+////		arreglo[1]=30;
+//		cout<<"arregloxxx:	"<<arreglo[1]<<endl;
 //			nc=140;
 //							cout<<"arregloxxxx:"<<endl;
 //								for(int i = 0; i< tam;i++){
@@ -226,16 +189,16 @@ void priorityQueue<T>::attendPriorityQueue(){
 //				}
 //		}	
 //	}	
-}
+//}
 
-template <class T>
-void priorityQueue<T>::organizarArreglo(){
-	NodoPQ<T> *ncambio2 = new NodoPQ<T>;
-	ncambio2 = cab;
-	for(int i = 0; i< tam;i++){
-		ncambio2->info= arreglo[i];
-		ncambio2 = ncambio2->sig;
-	}	
-}
+//template <class T>
+//void priorityQueue<T>::organizarArreglo(){
+//	NodoPQ<T> *ncambio2 = new NodoPQ<T>;
+//	ncambio2 = cab;
+//	for(int i = 0; i< tam;i++){
+//		ncambio2->info= arreglo[i];
+//		ncambio2 = ncambio2->sig;
+//	}	
+//}
 #endif
 
